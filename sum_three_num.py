0bae48be28e1e476384569cb12a0bc374ff008a7
ankush468 <42136801+ankush468@users.noreply.github.com>
2025-01-12 16:13:13 +0530

    Create sum_three_num.py

---
+++ sum_three_num.py
@@ -0,0 +1,12 @@
+#!/bin/bash
+#find all comination where a+b+c = 0
+
+array = [-1,0,1,2,-1,-4,3]
+
+new_array = []
+for i in range(len(array)):
+    for j in range(i+1,len(array)-1):
+        if (array[i]+array[j]+array[j+1]) == 0:
+            new_array.append([array[i],array[j],array[j+1]])
+        
+print(new_array)
